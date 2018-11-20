db610996d7fa9bfc8c4292c68028492e8068f33b
LinTao <lint@beanpop.cn>
2018-11-20 16:38:09 +0800

    添加基础文件

---
+++ MyObject/MyObject.h
@@ -0,0 +1,17 @@
+//
+//  MyObject.h
+//  PodTest
+//
+//  Created by 林涛 on 2018/11/20.
+//  Copyright © 2018 林涛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyObject : NSObject
+- (void)myPrint;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyObject/MyObject.m
@@ -0,0 +1,17 @@
+//
+//  MyObject.m
+//  PodTest
+//
+//  Created by 林涛 on 2018/11/20.
+//  Copyright © 2018 林涛. All rights reserved.
+//
+
+#import "MyObject.h"
+
+@implementation MyObject
+
+- (void)myPrint{
+    NSLog(@"这是一个测试");
+}
+
+@end
